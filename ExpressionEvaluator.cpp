61bc07c16b2c2cef9843f12cce63de7d48eda06c
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-03-14 01:53:05 +0100

    manage negative numbers

--- ExpressionEvaluator.cpp
+++ ExpressionEvaluator.cpp
@@ -4,13 +4,19 @@
 #include <unordered_map>
 
 std::string manageNegativeNumbers(std::string expression) {
-
 	std::string exp;
 	std::stack<char> stack;
 	for (auto& c : expression) {
 		if (c == '-') {
-			if (stack.empty() || std::ispunct(stack.top())) {
+			if (stack.empty()) {
+				stack.push(' ');
 				stack.push('0');
+			}
+			else {
+				if (std::ispunct(stack.top())) {
+					stack.push(' ');
+					stack.push('0');
+				}
 			}
 		}
 		if (c != ' ') {
@@ -22,6 +28,59 @@ std::string manageNegativeNumbers(std::string expression) {
 		stack.pop();
 	}
 	std::reverse(exp.begin(), exp.end());
+	std::string stringSt;
+	std::vector<std::string> postFix;
+	for (size_t i = 0; i < exp.size(); ++i) {
+		if (std::isdigit(exp.at(i))) {
+			while (std::isdigit(exp.at(i))) {
+				stringSt += exp.at(i);
+				++i;
+				if (i == exp.size()) {
+					break;
+				}
+			}
+			if (!stringSt.empty()) {
+				postFix.push_back(stringSt);
+				stringSt.erase(stringSt.begin(), stringSt.end());
+			}
+		}
+		if (i == exp.size()) {
+			break;
+		}
+		postFix.push_back(std::string(1, exp.at(i)));
+	}
+	for (size_t i = 0; i < postFix.size(); ++i) {
+		if (postFix.at(i) == " ") {
+			postFix.at(i) = "(";
+			for (size_t j = i + 1; j < postFix.size(); ++j) {
+				if (postFix.at(j).at(0) == '(') {
+					while (postFix.at(j).at(0) != ')') {
+						++j;
+					}
+					if (j < postFix.size() - 1) {
+						postFix.insert(postFix.begin() + (j + 1), std::string(1, ')'));
+					}
+					else {
+						postFix.push_back(std::string(1, ')'));
+					}
+					break;
+				}
+				if (std::isdigit(postFix.at(j).at(0)) && postFix.at(j).at(0) != '0') {
+					if (j < postFix.size()-1) {
+						postFix.insert(postFix.begin() + (j+1), std::string(1, ')'));
+					}
+					else {
+						postFix.push_back(std::string(1, ')'));
+					}
+					break;
+				}
+			}
+		}
+	}
+	for (auto& c : postFix) {
+		std::cout << c;
+	}
+	std::cout << std::endl;
 	return exp;
 }
 double calc(std::string expression) {
@@ -65,7 +124,7 @@ double calc(std::string expression) {
 				if (expression.at(i) == '/') stack.push("/");
 			}
 			else {
-				while (!stack.empty() && stack.top() != "(" && 
+				while (!stack.empty() && stack.top() != "(" &&
 					opMap[expression.at(i)] <= opMap[stack.top().at(0)]) {
 					postFix.push_back(stack.top()); //<-------
 					stack.pop();
@@ -84,7 +143,13 @@ double calc(std::string expression) {
 		postFix.push_back(stack.top());//<-------
 		stack.pop();
 	}//end conversion into postfix
-	double a = 0; double b = 0; double c = 0; 
+
+	for (auto& c : postFix) {
+		std::cout << c;
+	}
+	std::cout << std::endl;
+
+	double a = 0; double b = 0; double c = 0;
 	while (!postFix.empty()) {
 		if (!std::ispunct(postFix.front().at(0))) {
 			stack.push(postFix.front());
@@ -117,6 +182,6 @@ double calc(std::string expression) {
 	return std::stod(stack.top());
 }
 int main() {
-	std::cout << calc("3 -(-1)");
+	std::cout << calc("-7 * -(6 / 3)");
 	std::cin.ignore();
 }
