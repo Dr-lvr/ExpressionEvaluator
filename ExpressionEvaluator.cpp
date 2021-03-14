da567e2ec18d08b0bf64e12677fbaba89b3a8f8b
Davide <64044800+Dr-lvr@users.noreply.github.com>
2021-03-14 16:37:13 +0100

    adjust negative management

--- ExpressionEvaluator.cpp
+++ ExpressionEvaluator.cpp
@@ -78,6 +78,31 @@ std::string manageNegativeNumbers(std::string expression) {
 		}
 	}
 	stringSt.erase(stringSt.begin(), stringSt.end());
+	/*for (auto& c : inFix) {
+		stringSt += c;
+	}*/
+	std::stack<std::string> ok;
+	for (int i = 0; i < inFix.size(); ++i) {
+		std::cout << inFix.at(i);
+		if (!ok.empty()) {
+			if (ok.top() == ")" && inFix.at(i) == "(") {
+				ok.push(std::string(1, '+'));
+			}
+		}
+		ok.push(inFix.at(i));
+	}
+	std::cout << std::endl;
+	std::cout << std::endl;
+	inFix.erase(inFix.begin(), inFix.end());
+	while (!ok.empty()) {
+		inFix.push_back(ok.top());
+		ok.pop();
+	}
+	std::reverse(inFix.begin(), inFix.end());
+	for (auto& c : inFix) {
+		std::cout << c;
+	}
+	std::cout<<std::endl;
 	for (auto& c : inFix) {
 		stringSt += c;
 	}
@@ -85,15 +110,16 @@ std::string manageNegativeNumbers(std::string expression) {
 }
 double calc(std::string expression) {
 
+	std::unordered_map <char, int> opMap = { {'+', 1},{'-', 1},{'*', 2},{'/', 2} };
 	std::string stringSt;
 	std::stack<std::string> stack;
 	std::deque<std::string> postFix;
-	std::unordered_map <char, int> opMap = { {'+', 1},{'-', 1},{'*', 2},{'/', 2} };
 	std::ostringstream streamObj;
 	streamObj << std::fixed;
 	streamObj << std::setprecision(10);
 
 	expression = manageNegativeNumbers(expression);
+	//return 0;
 	for (int i = 0; i < (int) expression.size(); ++i) {
 		while (std::isdigit(expression.at(i)) || expression.at(i) == '.') {
 			stringSt += expression.at(i);
@@ -182,11 +208,15 @@ double calc(std::string expression) {
 	return std::stod(stack.top());
 }
 int main() {
+
+	std::cout << calc("(91.62+81.06*67.01)*-80.36*17.78+-29.13/((96.68-47.52+((59.91)/(14.82/(72.29+53.25))-42.83)*--50.08+--27.91*44.31+-38.52-3.1*49.28*-(69.01)-6.78/(75.18/(91.89))/(-(40.03))-(-77.49-62.22))-95.66/(-8.05/(6.47))+44.43*((61.86)--43.97+91.32+34.92+86.83+95.86*87.69+(13.36/(-74.02+77.65))+(60.97)/((42.12)+74.91))*4.4+29.84/(47.06))") << std::endl;
+	/*
 	std::cout << "prima operazione --- scritta correttamente ---> (0-7) * (0-(6 / 3)): " << std::endl;
 	std::cout << calc("(0-7) * (0-(6 / 3))") <<std::endl;
 	std::cout << "seconda operazione --- equivalente ---> -7 * -(6 / 3)" << std::endl;
 	std::cout << calc("-7 * -(6 / 3)") << std::endl;
 	std::cout << "must be -7891910.63605 . . . " << std::endl;
 	std::cout << calc("(91.62+81.06*67.01)*-80.36*17.78+-29.13/((96.68-47.52+((59.91)/(14.82/(72.29+53.25))-42.83)*--50.08+--27.91*44.31+-38.52-3.1*49.28*-(69.01)-6.78/(75.18/(91.89))/(-(40.03))-(-77.49-62.22))-95.66/(-8.05/(6.47))+44.43*((61.86)--43.97+91.32+34.92+86.83+95.86*87.69+(13.36/(-74.02+77.65))+(60.97)/((42.12)+74.91))*4.4+29.84/(47.06))");
+	*/
 	std::cin.ignore();
 }
